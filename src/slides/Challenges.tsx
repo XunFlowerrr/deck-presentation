a41620e3ef7980a70f903858ed730f5b34a58060
pinwap <156493711+pinwap@users.noreply.github.com>
2026-06-16 00:19:48 +0900

    feat: แก้เออเร่อ

--- src/slides/Challenges.tsx
+++ src/slides/Challenges.tsx
@@ -72,7 +72,6 @@ export function Challenges() {
           strokeWidth="2.5"
           strokeLinecap="round"
           strokeLinejoin="round"
-          align="center"
         >
           <line x1="18" y1="20" x2="18" y2="10" />
           <line x1="12" y1="20" x2="12" y2="4" />
